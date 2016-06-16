0f2789769c52964f0c68a5d1c76527116378385f
Eric W. Greene <eric@ericwgreene.com>
2016-06-15 20:51:19 -0700

    added unit testing code

---
+++ unit-testing/www/tests/specs/filters/capitalize-tests.js
@@ -0,0 +1,24 @@
+describe("capitalize filter tests", function() {
+
+	var capitalizeFlt;
+
+	beforeEach(angular.mock.module("WidgetApp"));
+
+	beforeEach(angular.mock.inject(function($filter) {
+
+		capitalizeFlt = $filter("capitalize");
+
+	}));
+
+	it("capitalize a string", function() {
+
+		var
+			testValue = "blue",
+			expectedValue = "Blue",
+			result = capitalizeFlt(testValue);
+
+		expect(result).toEqual(expectedValue);
+
+	});
+
+});
