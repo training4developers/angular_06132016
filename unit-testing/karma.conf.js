851711779660389f77d7d6196db5f41df7a30c9a
Eric W. Greene <eric@ericwgreene.com>
2016-06-16 12:49:28 -0700

    added coverage configuration

--- unit-testing/karma.conf.js
+++ unit-testing/karma.conf.js
@@ -41,13 +41,16 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {},
+    preprocessors: {
+			'www/js/!(ng)/*.js': ['coverage'],
+			'www/js/*.js': ['coverage'],
+		},
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots', 'junit'],
+    reporters: ['dots', 'junit', 'coverage'],
 
 		junitReporter: {
 			outputFile: 'test-results.xml'
